87fbf9a11f1d4caaebe741575b19fb1b7d74f6b8
drishya1998 <58319192+drishya1998@users.noreply.github.com>
2023-03-23 00:58:17 -0400

    updated app.py with initial webhook functions

--- app.py
+++ app.py
@@ -1,40 +1,193 @@
 from flask import Flask, jsonify, request
+import re
+import urllib.request
+import urllib.parse
+import pandas as pd
+import random
+
 # initialize our Flask application
-app= Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/song", methods=["GET"])
-def setName():
-    if request.method=='GET':
-        artist = request.args.get('artist').lower()
-        
-        if artist=="taylor swift":
-            songs = ["Shake It Off", "You Belong With Me", "Blank Space", "Love Story", "I Knew You Were Trouble."]
-        
-        elif artist=="eminem":
-            songs = ["Superman", "Lose Yourself", "Venom", "Rap God", "Without Me"]
 
-        elif artist=="ed sheeran":
-            songs = ["Shape of You", "Bad Habits", "Perfect", "Castle on the Hill", "Galway Girl"]
+#Function for recommending other songs by a given artist
+def songByArtist(content):
+  artist = content['queryResult']['parameters']['music-artist'].lower()
+  df = pd.read_csv('music_db.csv')
+  #query the df to get the songs by a particular artist
+  #output shows as [['shake it off'], ['you belong with me'], ['blank space'],...]]
+  songs_by_artist = df.loc[df.artist.str.contains(artist),
+                           ['song']].values.tolist()
+  msg = ""
+  if len(songs_by_artist) > 0:
+    random.shuffle(songs_by_artist)
+    songs = []
+    
+    for r in songs_by_artist:
+      if len(songs_by_artist) > 3 and len(songs) < 3:   #for artists with more than 3 songs in the csv, list out just 3 songs
+        songs.append(r[0].title())  #to get the list in the correct format
+      elif len(songs_by_artist) < 3:    #for artists with less than 3 songs in the csv
+        songs.append(r[0].title())
+      
+      
+    if len(songs) > 0:   #if we know any songs by the artist
+        msg = "Here are a few songs by " + artist.title() 
+        for i in songs:
+            msg+=", "+i
 
-        elif artist=="sia":
-            songs = ["Unstoppable", "Genius", "Chandelier", "The Greatest", "I'm Still Here"]
+  else:   #if we don't know the artist
+        msg = "I don't have any suggestions for this artist, sorry!"
 
-        elif artist=="akon":
-            songs = ["Chammak Challo", "Smack That", "I Wanna Love You", "Beautiful", "Lonely"]
+  fulfilment_response = {"fulfillmentMessages": [{"text": {"text": [msg]}}]}
+  return jsonify(fulfilment_response)
 
-        elif artist=="linkin park":
-            songs = ["Numb", "Faint", "New Divide", "In the End", "Breaking the Habit"]
 
-        elif artist=="metallica":
-            songs = ["Enter Sandman", "One", "Master of Puppets", "For Whom The Bell Tolls", "Fade to Black"]
+#Function for recommending popular song by artist
+def popularSong(content):
+  artist = content['queryResult']['parameters']['music-artist'].lower()
+  df = pd.read_csv('music_db.csv')
+  #query the df to get the songs by a particular artist where the popularity column is set as 1
+  songs_by_artist = df.loc[(df.artist.str.contains(artist)) &
+                           (df['popularity'] == 1), ['song']].values.tolist()
+  if len(songs_by_artist) > 0:
+    msg = songs_by_artist[0][0].title() + " is one of the popular songs by " + artist
 
-        elif artist=="harry styles":
-            songs = ["As It Was", "Watermelon Sugar"]
-        
-        else:
-            songs = "No Suggestions for the arist provided"
-        
-        return jsonify({"songs":songs})
+  else:
+    msg = "I'm sorry, I haven't heard of that artist."
 
-if __name__=='__main__':
-    app.run()
+  fulfilment_response = {"fulfillmentMessages": [{"text": {"text": [msg]}}]}
+  return jsonify(fulfilment_response)
+
+
+
+
+#Function for recommending song by genre
+def songByGenre(content):
+  genre = content['queryResult']['parameters']['music-genre'].lower()
+  df = pd.read_csv('music_db.csv')
+  #query the df to get the songs by a particular genre
+  songs_list = df.loc[df.genre.str.contains(genre), ['song']].values.tolist()
+  random.shuffle(songs_list)
+  if len(songs_list) > 0:
+    msg = songs_list[0][0].title() + " is one of the popular " + genre + " songs right now."
+
+  else:
+    msg = "I'm sorry, I don't have any data for that genre."
+
+  fulfilment_response = {"fulfillmentMessages": [{"text": {"text": [msg]}}]}
+  return jsonify(fulfilment_response)
+
+
+
+
+#Function for recommending song by mood
+def songByMood(content):
+  mood = content['queryResult']['parameters']['mood'].lower()
+  df = pd.read_csv('music_db.csv')
+  #query the df to get the songs by a particular mood
+  songs_list = df.loc[df.mood.str.contains(mood), ['song']].values.tolist()
+  random.shuffle(songs_list)
+  if len(songs_list) > 0:
+    msg = songs_list[0][0].title() + " is one of the popular " + mood + " songs right now."
+
+  else:
+    msg = "I'm sorry, I don't have any data for that mood."
+
+  fulfilment_response = {"fulfillmentMessages": [{"text": {"text": [msg]}}]}
+  return jsonify(fulfilment_response)
+
+
+
+#Function for recommending albums by artist
+def getAlbum(content):
+  artist = content['queryResult']['parameters']['music-artist'].lower()
+  df = pd.read_csv('music_db.csv')
+  #query the df to get the albums by a particular artist
+  albums_by_artist = df.loc[df.artist.str.contains(artist),
+                           ['album']].values.tolist()
+  msg = ""
+  if len(albums_by_artist) > 0:
+    random.shuffle(albums_by_artist)
+    albums = []
+    
+    for r in albums_by_artist:
+      if len(albums_by_artist) > 3 and len(albums) < 3:   #for artists with more than 3 albums in the csv, list out just 3 albums
+        albums.append(r[0].title())  #to get the list in the correct format
+      elif len(albums_by_artist) < 3:    #for artists with less than 3 albums in the csv
+        albums.append(r[0].title())
+      
+      
+    if len(albums) > 0:   #if we know any albums by the artist
+        msg = "Here are a few albums by " + artist.title() 
+        for i in albums:
+            msg+=", "+i
+
+  else:   #if we don't know the artist
+        msg = "I don't have any suggestions for this artist, sorry!"
+
+  fulfilment_response = {"fulfillmentMessages": [{"text": {"text": [msg]}}]}
+  return jsonify(fulfilment_response)
+
+
+
+
+#Function for getting YT link of a song
+def getYTLink(content):
+  track = content['queryResult']['parameters']['song_name'].lower()
+  artist = content['queryResult']['parameters']['music-artist'].lower()
+  input = urllib.parse.urlencode({'search_query': track + ' by ' + artist})
+  html = urllib.request.urlopen("http://www.youtube.com/results?" + input)
+  try:
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    fulfilment_response = {
+      "fulfillmentMessages": [{
+        "text": {
+          "text": [
+            "Sure, here is the link to the music video for " + track.title() +
+            " by " + artist.title() + "\n\n" +
+            "https://www.youtube.com/watch?v=" + video_ids[0]
+          ]
+        }
+      }]
+    }
+    return jsonify(fulfilment_response)
+  except:
+    fulfilment_response = {
+      "fulfillmentMessages": [{
+        "text": {
+          "text": [
+            "Sorry, I couldn't find that song on YouTube. Try again with a different song or artist"
+          ]
+        }
+      }]
+    }
+    return jsonify(fulfilment_response)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+  return "App is running"
+
+
+@app.route('/redirect', methods=["POST"])
+def redirectToURLs():
+  content = request.json
+  intent = content['queryResult']['intent']['displayName'].lower()
+  resp = {}
+  if intent == 'youtube link':
+    resp = getYTLink(content)
+  elif intent == 'songbyartist':
+    resp = songByArtist(content)
+  elif intent == 'popular-song':
+    resp = popularSong(content)
+  elif intent == 'genres':
+    resp = songByGenre(content)
+  elif intent == 'mood':
+    resp = songByMood(content)
+  elif intent == 'album':
+    resp = getAlbum(content)
+  return resp
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
